a84ef9ef0ae6dddf740b8b1bf52f326db1bd34fb
nandakishore-k <nandakishorek2002@gmail.com>
2024-04-05 22:30:13 +0530

    firsst commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { routes } from './app.routes';
+import { firebaseConfig } from './core/constants/constants';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      AngularFireModule.initializeApp(firebaseConfig),
+     AngularFireAuthModule,
+     AngularFirestoreModule,
+     AngularFireDatabaseModule
+    
+    ])]
 };
---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCX3D6AfHibLKQSa-Q6QDfnaNv5xCZPxss",
+    authDomain: "expense-tracker-com.firebaseapp.com",
+    databaseURL: "https://expense-tracker-com-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "expense-tracker-com",
+    storageBucket: "expense-tracker-com.appspot.com",
+    messagingSenderId: "979907193897",
+    appId: "1:979907193897:web:aced0dc411ede9fab9c038"
+  };
